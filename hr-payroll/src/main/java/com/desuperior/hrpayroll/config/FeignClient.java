29513a25e3a941be3bbb35e8af2b59055ecde7a5
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2022-06-26 18:55:20 +0200

    Eureka client has been implemented

--- hr-payroll/src/main/java/com/desuperior/hrpayroll/config/FeignClient.java
+++ hr-payroll/src/main/java/com/desuperior/hrpayroll/config/FeignClient.java
@@ -1,13 +1,15 @@
 package com.desuperior.hrpayroll.config;
 
 import com.desuperior.hrpayroll.entities.Worker;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@org.springframework.cloud.openfeign.FeignClient(name = "worker", url = "localhost:8001", path = "/workers")
+
+@org.springframework.cloud.openfeign.FeignClient(name = "hr-worker", path = "/workers")
 public interface FeignClient {
 
     @GetMapping(path = "/{id}")
--- hr-payroll/src/main/java/com/desuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/desuperior/hrpayroll/services/PaymentService.java
@@ -4,6 +4,7 @@ import com.desuperior.hrpayroll.config.FeignClient;
 import com.desuperior.hrpayroll.entities.Payment;
 import com.desuperior.hrpayroll.entities.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,7 +12,7 @@ import java.util.Optional;
 @Service
 public class PaymentService {
 
-
+    @LoadBalanced
     private final FeignClient feignClient;
 
     @Autowired
