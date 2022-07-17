c388ca23cd338a07aad8d434de35281943b12c38
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2022-07-17 11:27:34 +0200

    Configured API Gateway
    - Configured services routes of hr-worker and hr-payroll

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayApplication.java
@@ -3,12 +3,15 @@ package com.devsuperior.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
-public class HrApiGatewayZuulApplication {
+public class HrApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+        SpringApplication.run(HrApiGatewayApplication.class, args);
     }
 
 }
