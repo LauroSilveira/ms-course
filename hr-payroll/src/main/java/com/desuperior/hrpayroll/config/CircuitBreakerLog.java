905b47abdf121ccc45b0893886abb0cc8e6be0f8
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2022-07-17 22:46:45 +0200

    Add circuit breaker class configutation

---
+++ hr-payroll/src/main/java/com/desuperior/hrpayroll/config/CircuitBreakerLog.java
@@ -0,0 +1,44 @@
+package com.desuperior.hrpayroll.config;
+
+
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CircuitBreakerLog {
+
+    private static final  Logger CIRCUIT_BREAKER_LOG = LoggerFactory.getLogger(CircuitBreakerLog.class);
+
+    //this class has the responsibility to log the states of circuit breaker
+    @Bean
+    private RegistryEventConsumer<CircuitBreaker> circuitBreakerLog() {
+
+        return new RegistryEventConsumer<CircuitBreaker>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry()
+                        .getEventPublisher()
+                        .onStateTransition(state -> CIRCUIT_BREAKER_LOG.info(state.getStateTransition().name()));
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+
+            }
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+
+            }
+        };
+
+    }
+}
