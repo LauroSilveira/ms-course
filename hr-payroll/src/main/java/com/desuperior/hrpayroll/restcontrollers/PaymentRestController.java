2ea9c365a2dc7d7e5a158daa8565d370865e285b
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2022-07-17 22:17:33 +0200

    Setup Resilience4J and add Logs.

--- hr-payroll/src/main/java/com/desuperior/hrpayroll/restcontrollers/PaymentRestController.java
+++ hr-payroll/src/main/java/com/desuperior/hrpayroll/restcontrollers/PaymentRestController.java
@@ -3,6 +3,7 @@ package com.desuperior.hrpayroll.restcontrollers;
 import com.desuperior.hrpayroll.entities.Payment;
 import com.desuperior.hrpayroll.services.PaymentService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentRestController {
@@ -24,26 +26,21 @@ public class PaymentRestController {
         this.service = service;
     }
 
-    @CircuitBreaker(name = "worker", fallbackMethod = "getPaymentCircuitBreaker")
+    @CircuitBreaker(name = "hr-worker", fallbackMethod = "paymentCircuitBreaker")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        log.info("Request to getPayment with workerId {}, days {} ", workerId, days);
         Optional<Payment> payment = Optional.ofNullable(service.getPayment(workerId, days));
         return payment.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 
     }
 
-    /**
-     * FallBack method of getPayment
-     *
-     * @param workerId
-     * @param days
-     * @return a Response Entity of Payment
-     */
-    public ResponseEntity<Payment> getPaymentCircuitBreaker(Long workerId, Integer days, Exception ex) {
-        return ResponseEntity.ok(Payment.builder().name("test").day(2)
-                .dailyIncome(10.0)
+    //FallBack method of getPayment
+    public ResponseEntity<Payment> paymentCircuitBreaker(Long workerId, Integer days, Exception ex) {
+        log.info("Circuit breaker Opened, response will be the request");
+        return ResponseEntity.ok(Payment.builder().name(workerId.toString()).day(days)
+                .dailyIncome(days.doubleValue())
                 .build());
-
     }
 }
--- hr-payroll/src/main/java/com/desuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/desuperior/hrpayroll/services/PaymentService.java
@@ -3,6 +3,7 @@ package com.desuperior.hrpayroll.services;
 import com.desuperior.hrpayroll.config.FeignClient;
 import com.desuperior.hrpayroll.entities.Payment;
 import com.desuperior.hrpayroll.entities.Worker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import javax.crypto.spec.OAEPParameterSpec;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class PaymentService {
 
     @LoadBalanced
@@ -22,12 +24,13 @@ public class PaymentService {
     }
 
     public Payment getPayment(Long workerId, int days) {
+        log.info("Request to Microservice Worker with parameters: Id {}, days {}", workerId, days);
         Optional<Worker> response = Optional.ofNullable(feignClient.findById(workerId).getBody());
-
+        log.info("Response of microservice Worker with Feign: {}", response.map(Worker::getId));
         return response.map(worker -> Payment.builder()
                 .name(worker.getName())
                 .dailyIncome(worker.getDailyIncome())
                 .day(days)
-                .build()).orElse(null);
+                .build()).orElse(Payment.builder().build());
     }
 }
